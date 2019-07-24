45e332218b33632de840c8e2aef03ecb637b479a
Jorge Ponce <daraciel@gmail.com>
2019-07-24 18:10:04 +0200

    agregados niveles

---
+++ Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelHandler : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        string currentSceneName = string.Empty;
+        string nextSceneName = string.Empty;
+
+        currentSceneName = SceneManager.GetActiveScene().name;
+
+        switch(currentSceneName)
+        {
+            case "Level_1":
+                nextSceneName = "Level_2";
+                break;
+            case "Level_2":
+                nextSceneName = "Level_1";
+                break;
+
+        }
+
+        if(!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.Log("Unknown transaction from scene " + currentSceneName);
+        }
+    }
+}
